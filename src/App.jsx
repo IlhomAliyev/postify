d4be8c21a35effe0533c14faf48c19d1ac5383fc
Ilhom Aliyev <aliyevilhom99@gmail.com>
2023-05-14 18:13:49 +0500

    themeSwitch func optimization

--- src/App.jsx
+++ src/App.jsx
@@ -10,28 +10,15 @@ import darkBg from './assets/dark-bg.jpg';
 
 const App = () => {
   //! === Page Theme ===
-  // const date = new Date();
-  // const hour = date.getHours();
-
-  // hour >= 18 || hour <= 5
-  //   ? (document.body.className = '_dark')
-  //   : (document.body.className = '');
 
   const appBody = document.body;
   const [darkTheme, setDarkTheme] = useState(false);
 
-  // const [bodyTheme, setBodyTheme] = useState((document.body.className = ''));
-  // const [bgImage, setBgImage] = useState('./assets/light-bg.jpg');
-
   const themeSwitch = () => {
     darkTheme ? setDarkTheme(false) : setDarkTheme(true);
-    // document.body.className !== '_dark'
-    //   ? setBodyTheme((document.body.className = '_dark'))
-    //   : setBodyTheme((document.body.className = ''));
   };
 
-  let bgImage = './assets/light-bg.jpg';
-
+  let bgImage = '';
   if (darkTheme) {
     bgImage = darkBg;
     appBody.className = '_dark';
@@ -46,14 +33,6 @@ const App = () => {
   useEffect(() => {
     hour >= 18 || hour <= 5 ? setDarkTheme(true) : setDarkTheme(false);
   }, []);
-
-  // document.body.classList.contains('_dark')
-  //   ? setBgImage('./assets/dark-bg.jpg')
-  //   : setBgImage('./assets/light-bg.jpg');
-
-  // useEffect(() => {
-  //   themeSwitch();
-  // }, [bodyTheme]);
 
   //! === Page Theme ===
 
